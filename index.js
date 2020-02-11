97f09be1594c57740ffe4a56e743a68320dc027f
Francois Jordaan <francois.jordaan@SAMAC139.local>
2020-02-11 22:47:59 +0200

    Initial commit of mvp

---
+++ index.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const fs = require('fs');
+const csv = require('csvtojson');
+
+const inputFilePath = path.join(__dirname, 'ToConvert', 'BMOGAM_fundManagers.csv');
+const outputFilePath = path.join(__dirname, 'Converted', 'converted-data.json');
+const finalFilePath = path.join(__dirname, 'Converted', 'fundmanagersUpsert.json');
+
+const convertToJson = async () => {
+	let stringObjectArray = await csv({delimiter: "|"}).fromFile(inputFilePath);
+
+	fs.writeFile(outputFilePath, JSON.stringify(stringObjectArray, null, 2), (err) => {
+		if (err) throw err;
+		console.log('File converted!');
+	})
+
+	fs.readFile(outputFilePath, 'utf8', (err, data) => {
+		if (err) throw err;
+
+		let refinedData = JSON.parse(data);
+
+		for (let key in refinedData) {
+			if (refinedData.hasOwnProperty(key)) {
+				delete refinedData[key]['Image'];
+				refinedData[key]['code'] = refinedData[key]['Code'];
+				delete refinedData[key]['Code'];
+				refinedData[key]['name'] = refinedData[key]['Name'];
+				delete refinedData[key]['Name'];
+				refinedData[key]['biography'] = [];
+				refinedData[key]['relatedEntities'] = refinedData[key]['Related Entities'].split(',');
+				delete refinedData[key]['Related Entities'];
+				refinedData[key]['headshot'] = "";
+				refinedData[key]['headshotContentType'] = "image/jpeg";
+				refinedData[key]['extended'] = {'title': refinedData[key]['Title']};
+				delete refinedData[key]['Title'];
+			}
+		}
+
+		fs.writeFile(finalFilePath, JSON.stringify(refinedData, null, 2), (err) => {
+			if (err) throw err;
+		});
+	})
+}
+
+convertToJson();
